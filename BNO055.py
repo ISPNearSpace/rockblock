ca01b28df61161bc908fb778fece3a003828035b
Diego Estevez <imdiegoestevez@users.noreply.github.com>
2018-11-17 02:03:13 -0500

    Update BNO055.py

--- BNO055.py
+++ BNO055.py
@@ -8,22 +8,19 @@ sensor = adafruit_bno055.BNO055(i2c)
 
 def get_data():
 
-    def accRead(): #m/s^2
-        acce = float('{0:.1f}'.format(sensor.accelerometer))
-        return acce
-
-    def gyroRead(): #deg/sec
-        gyro = float('{0:.1f}'.format(sensor.gyroscope))
-        return gyro
-
-    accelerometer = float(accRead())
-    gyroscope = float(gyroRead())
-
+    acce = float('{0:.1f}'.format(sensor.accelerometer))
+    gyro = float('{0:.1f}'.format(sensor.gyroscope))
+    magn = sensor.magnetometer
+    euler = sensor.euler
+    quatern = sensor.quaternion
+    lin_acc = sensor.linear_acceleration
+    grav = sensor.gravity    
+    
     try:
         with open('BNO055.csv', 'a') as f:
             thewriter = csv.writer(f)
             thewriter.writerow(
-                ["Accelerometer (m/s^2) =", accelerometer, "Gyroscope (deg/sec) = ", gyroscope])
+                ["Accelerometer (m/s^2) =", accelerometer, "Magnetism =", magn, "Euler ="sensor.euler, "Quaternion =",quatern,"Linear Acceleration =",lin_acc, "Gravity =", grav, "Gyroscope (deg/sec) = ", gyroscope])
     except:
         pass
 
